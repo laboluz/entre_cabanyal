b420be358dad79d2d885531ff9748a8f966db194
laboluz <laboluz.upv@gmail.com>
2021-05-06 19:00:23 +0200

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,18 @@
+#pragma mark once
+
+#define PACKAGE	"Entre Cabanyal"
+#define	VERSION	"0.0.1"
+#define DESCRIPTION "Interactive video overlay image in real-time and prerecorded video-image. The image viewer appears sharing talks about the problem of Cabanyal, Valencia."
+#define EC_WWW "https://laboluz.webs.upv.es/projects/entre_cabanyal/"
+#define EC_DEVELOPERS "Emanuele Mazza"
+#define TAGS "cabanyal,interactive installation,programming,code,interactivity"
+
+#define WINDOW_TITLE "Entre Cabanyal 0.0.2"
+
+#define WINDOW_WIDTH 3200 //1920+1280
+#define WINDOW_HEIGHT 1080
+
+#define PRIMARY_SCREEN_W 1920
+#define PRIMARY_SCREEN_H 1080
+#define SECONDARY_SCREEN_W 1280
+#define SECONDARY_SCREEN_H 720
